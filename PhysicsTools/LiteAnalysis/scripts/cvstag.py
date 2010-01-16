c28137e22b0363344112cab273a8ca14541cdc0f
prosper <>
2010-01-16 21:32:33 +0000

    new version

--- PhysicsTools/LiteAnalysis/scripts/cvstag.py
+++ PhysicsTools/LiteAnalysis/scripts/cvstag.py
@@ -1,61 +1,74 @@
 #!/usr/bin/env python
-#----------------------------------------------------------------------------
-# File: cvstag.py
-# Description: For each specified module perform a cvs rtag
-# Created: 13-Jun-2006, Harrison B. Prosper
-#$Revision: 1.2 $
-#----------------------------------------------------------------------------
-import sys, os, re
+#-----------------------------------------------------------------------------
+# tag files listed in given file, whose default name is MANIFEST
+#$Revision:$ HBP
+#-----------------------------------------------------------------------------
+import os, sys, re
 from string import *
-#----------------------------------------------------------------------------
-if not os.environ.has_key('PYTHON_PROJECTS'):
-    print "PYTHON_PROJECTS not defined"
-    sys.exit(1)
+from getopt import getopt, GetoptError
+#-----------------------------------------------------------------------------
+skipdir = re.compile(r'dataformats/lis|dataformats/txt|src/DataFormats')
+skipext = re.compile(r'\.([vV][1-9]+|so|log|old|gif|root|pyc|d|#|~)$')
+skipfile= re.compile(r'CVS|MANIFEST|\.cpp.h|\.header.h|a.txt|'\
+					  '\.Bayespack.out|log|XmlMap.py.*')
+SHORTOPTIONS = 'ha'
+def usage():
+	print """
+Usage:
+    cvstag.py [options] <tagname> [MANIFEST]
 
-names = {}
-names['project'] = split(os.environ['PWD'],'/').pop()
-names['base']    = os.environ['PYTHON_PROJECTS']
-names['config']  = 'config/' % names
+	options
+	     -h     print this
+	     -a     tag all files
 
-# Make sure we're in the right directory
+                default - skip files in:
+				dataformats/lis
+				dataformats/txt
+				src/DataFormats
+	"""
+	sys.exit(0)
+#-----------------------------------------------------------------------------
+# Decode command line
+	
+try:
+	opts, other = getopt(sys.argv, SHORTOPTIONS)
+except GetoptError, m:
+	print
+	print m
+	usage()
 
-if not os.path.exists('%(config)s' % names):
-    print "\ncvstag must be run from a project directory!\n"
-    sys.exit(1)
-    
-#----------------------------------------------------------------------------
-def newversion(n):
-    a = 100*n[0]+10*n[1]+n[2] + 1
-    i = a / 100
-    j = (a - i * 100) / 10
-    k = (a - i * 100 - j * 10)
-    return (i,j,k)
+# Decode option, value pairs
 
-curversion = re.compile(r'(?<=\[)[\w_]+(?=:A\])')
-number = re.compile('[0-9]+')
-command = "cvs history -m%(project)s" % names
-recs = os.popen(command).readlines()
-if recs[0][0:6] == 'No rec':
-    version = '[v1_0_0:A]'
+skip = True
+for option, value in opts:
+	if option == "-h":
+		usage()
+		
+	elif option == "-a":
+		skip = False
+			
+other = other[1:]
+if len(other) == 0: usage()
+
+tagname = other[0]
+if len(other) > 1:
+	manifest = other[1]
 else:
-    version = strip(recs[-2])
-cver = curversion.findall(version)[0]
-n = map(atoi, number.findall(cver))
-m = newversion(n)
-names['version'] = 'v%d_%d_%d' % m
-    
-command = '''
-cvs commit -m"tag with %(version)s"
-''' % names
-if find(os.popen(command).read(),'failed') > -1:
-    print "cvs commit failed - please fix errors!"
-    sys.exit(0)
-
-command = '''
-cvs rtag %(version)s %(project)s;
-''' % names
-os.system(command)
-print "%(project)s - new tag: %(version)s" % names
-open("%(base)s/%(project)s/VERSION" % names, 'w').write('%(version)s\n'% names)
-
-    
+	manifest = "MANIFEST"
+if not os.path.exists(manifest):
+	print "file %s not found" % manifest
+	sys.exit(0)
+	
+filenames = map(strip, open(manifest))
+for filename in filenames:
+	if filename == ".": continue
+	if skipfile.search(filename) != None: continue
+	if skipext.search(filename)  != None: continue
+	
+	if skip:
+		if skipdir.search(filename) != None: continue
+	
+	command = "cvs tag %s %s" % (tagname, filename)
+	print command
+	os.system(command)
+	
