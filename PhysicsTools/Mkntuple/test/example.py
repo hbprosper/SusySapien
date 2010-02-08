6156fef0a0bb5995cc7dbe7cbadfe8851bc9f20f
prosper <>
2010-02-08 04:53:47 +0000

    added a bit of doc

---
+++ PhysicsTools/Mkntuple/test/example.py
@@ -0,0 +1,101 @@
+#$Revision: 1.4 $ example.py
+#------------------------------------------------------------------------------
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("Demo")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+
+process.source = cms.Source("PoolSource",
+							fileNames =
+							cms.untracked.vstring("file:pat.root"
+												  )
+							)
+
+#-------------------------------------------------------------------------
+# Created: Sun Feb  7 22:35:29 2010 by mkntuplecfi.py
+# $Revison:$
+#-------------------------------------------------------------------------
+process.demo =\
+cms.EDAnalyzer("Mkntuple",
+
+			   # Name of output n-tuple
+               ntupleName = cms.untracked.string("ntuple.root"),
+
+			   # List of buffers to allocate.
+			   #----------------------------------------------------------
+			   # The names are arbitrary so long as each agrees with
+			   # the name of a vstring. For example, since the vstring
+			   # "buffers" contains the name "recoBeamSpot", the code
+			   # expects a vstring of that name to exist.
+			   #----------------------------------------------------------
+               buffers =
+               cms.untracked.
+               vstring(
+    "recoBeamSpot",
+    "patMuon",
+    "recoGenParticle"
+    ),
+			   #----------------------------------------------------------
+			   # Format of 1st line:
+			   #   buffer-name         getByLabel         maxiumum-count
+			   #
+			   # Format of subsequent lines:
+			   #   [return-type] method [alias]
+			   #----------------------------------------------------------
+               recoBeamSpot =
+               cms.untracked.
+               vstring(
+    "recoBeamSpot                    offlineBeamSpot                   1",
+    #---------------------------------------------------------------------
+    " double   x0()",
+    " double   y0()",
+    " double   z0()"
+    ),
+               patMuon =
+               cms.untracked.
+               vstring(
+    "patMuon                         cleanLayer1Muons                500",
+    #---------------------------------------------------------------------
+    "    int   charge()",
+    " double   energy()",
+    " double   et()",
+    " double   eta()",
+    " double   p()",
+    " double   phi()",
+    " double   pt()",
+    "  float   caloIso()",
+    "  float   ecalIso()",
+	# It is possible to call the following, so long as the 2nd method is simple
+    " double   ecalIsoDeposit()->candEnergy()",
+    "  float   hcalIso()",
+    "  float   trackIso()",
+    "   bool   isCaloMuon()",
+    "   bool   isGlobalMuon()",
+    "   bool   isStandAloneMuon()",
+    "   bool   isTrackerMuon()"
+    ),
+               recoGenParticle =
+               cms.untracked.
+               vstring(
+    "recoGenParticle                 genParticles                     100",
+    #---------------------------------------------------------------------
+    "    int   charge()",
+    " double   energy()",
+    " double   et()",
+    " double   eta()",
+    " double   p()",
+    " double   phi()",
+    " double   pt()",
+    " double   px()",
+    " double   py()",
+    " double   pz()",
+    " double   mass()",
+    "    int   pdgId()",
+    "    int   status()"
+    )
+               )
+
+process.p = cms.Path(process.demo)
