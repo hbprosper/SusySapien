c92b56a05efd8ba50e4209418cb0096f7aa0e40d
prosper <>
2010-02-08 06:30:34 +0000

    missing link

---
+++ PhysicsTools/LiteAnalysis/src/ThingFactory.cc
@@ -0,0 +1,6 @@
+#include "PhysicsTools/LiteAnalysis/interface/ThingFactory.h"
+//-----------------------------------------------------------------------------
+ThingBase::~ThingBase() {}
+
+EDM_REGISTER_PLUGINFACTORY(ThingFactory, "ThingFactory");
+
