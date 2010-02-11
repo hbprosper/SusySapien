0c9ce6884ea41741bcc9b9a1a221143714d92bb8
prosper <>
2010-02-11 04:21:29 +0000

    make consistent with v3_4_1b of Mkntuple

--- SO10/Analysis/plugins/myplugins.cc
+++ SO10/Analysis/plugins/myplugins.cc
@@ -1,13 +1,11 @@
 //-----------------------------------------------------------------------------
 // Package:     SO10
 // Sub-Package: Analysis
-//$Revision: 1.2 $
+//$Revision: 1.3 $
 //-----------------------------------------------------------------------------
 #include "PhysicsTools/Mkntuple/interface/Buffer.h"
 #include "SO10/Analysis/interface/myplugins.h"
 //-----------------------------------------------------------------------------
-typedef Buffer<pat::Muon, patMuonExtra> patMuonExtra_t;
+typedef UserBuffer<pat::Muon, patMuonExtra> patMuonExtra_t;
 DEFINE_EDM_PLUGIN(BufferFactory, patMuonExtra_t, "patMuonExtra");
 
-typedef Buffer<reco::Muon, recoMuonIsol> recoMuonIsol_t;
-DEFINE_EDM_PLUGIN(BufferFactory, recoMuonIsol_t, "recoMuonIsol");
--- SO10/Analysis/test/config.py
+++ SO10/Analysis/test/config.py
@@ -1,4 +1,4 @@
-#$Revision: 1.2 $
+#$Revision: 1.4 $
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("Demo")
@@ -9,9 +9,9 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.source = cms.Source("PoolSource",
 							fileNames =
-							cms.untracked.vstring("file:minbiasdata.root"
+							cms.untracked.vstring("file:pat.root"
 												  )
 							)
-process.load("SO10.Analysis.mkntuple_cfi")
+process.load("SO10.Analysis.pat_cfi")
 
 process.p = cms.Path(process.demo)
