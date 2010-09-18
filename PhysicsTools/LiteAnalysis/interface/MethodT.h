895690298161fcf74a0f459fa5655bd11c0738e0
prosper <>
2010-09-18 21:01:08 +0000

    split MethodT into two classes

--- PhysicsTools/LiteAnalysis/interface/MethodT.h
+++ PhysicsTools/LiteAnalysis/interface/MethodT.h
@@ -1,289 +1,71 @@
 #ifndef METHODT_H
 #define METHODT_H
+//-----------------------------------------------------------------------------
 //
 // Package:    PhysicsTools/LiteAnalysis
 //             MethodT.h
 //
 // Original Author:  Harrison B. Prosper
 //         Created:  Tue Dec  8 15:40:26 CET 2009
-// $Id: MethodT.h,v 1.1 2010/09/03 04:56:58 prosper Exp $
+// $Id: MethodT.h,v 1.2 2010/09/06 05:28:48 prosper Exp $
 //
+//-----------------------------------------------------------------------------
 // If using Python, include its header first to avoid annoying compiler
 // complaints.
 #include <Python.h>
 #include <boost/python/type_id.hpp>
-#include <boost/regex.hpp>
-#include <iostream>
-#include <sstream>
-#include <string>
-#include <cassert>
-#include <stdlib.h>
-
-#include "FWCore/Framework/interface/Frameworkfwd.h"
-#include "FWCore/Framework/interface/Event.h"
-#include "PhysicsTools/LiteAnalysis/interface/kit.h"
 #include "CommonTools/Utils/src/ExpressionPtr.h"
 #include "CommonTools/Utils/src/ExpressionBase.h"
 #include "CommonTools/Utils/interface/expressionParser.h"
-#include "TROOT.h"
-
-// This class models functions of the form
-//
-//  <simple-return-type> function(<simple-type>)
-//
-// where <simple-return-type> is a simple type like float and <simple-type> 
-// is typically void. The function could be compound, for example:
-// y = o->track()->pt()
-
+#include "FWCore/Utilities/interface/Exception.h"
+#include "PhysicsTools/LiteAnalysis/interface/MethodTBase.h"
+#include "Reflex/Object.h"
+#include "Reflex/Type.h"
+//-----------------------------------------------------------------------------
 template <typename T>
-struct MethodT 
+reco::parser::ExpressionPtr parserPtr(std::string expression)
+{
+  std::string delim("");
+  reco::parser::ExpressionPtr ptr;
+  if ( !isCompoundMethod(expression, delim) )
+    if( !reco::parser::expressionParser<T>(expression, ptr) ) 
+      {
+        throw cms::Exception("ExpressionError",
+                             "Since I'm a cyber ignoramous, "
+                             "I'm too stupid to understand \"" 
+                             + expression "\"\n");
+      } 
+  return ptr;
+}
+
+/** Model function members, that is, methods.
+    The functions that 
+    can be modeled must return simple types and have either no arguments or
+    arguments comprising simple types. The functions can be of the form:
+    - y = method1(...)->method2(...) or
+    - y = method1(...).method2(...) or
+    - y = method1(..).variable
+*/
+template <typename T>
+struct MethodT : public MethodTBase 
 {
   MethodT() {}
   
+  ///
   MethodT(const std::string expression)
-    : expression_(expression),
-      expression1_(""),
-      expression2_(""),
-      classname_(boost::python::type_id<T>().name()),
-      type_(ROOT::Reflex::Type::ByTypeInfo(typeid(T))),
-      compoundMethod_(false),
-      checkReturn_(false)
-  {
-    if ( getenv("DEBUGMETHOD") > 0 )
-      debug_ = atoi(getenv("DEBUGMETHOD"));
-    else
-      debug_ = 0;
+    : MethodTBase(boost::python::type_id<T>().name(),
+                  expression,
+                  parserPtr<T>(expression)),
+      type_(ROOT::Reflex::Type::ByTypeInfo(typeid(T)))
+  {}
 
-    // method could be of the form
-    // y = method1(...)->method2(...) or
-    // y = method1(...).method2(...) or
-    // y = method1(..).variable
-    boost::regex expr("(?<=[)]) *([-][>]|[.]) *(?=[a-zA-Z])");
-    boost::smatch what;
-
-    if ( debug_ > 0 )
-      std::cout << "Expression( " 
-                << BLUE << expression_ <<  BLACK << " )" << std::endl;
-
-    compoundMethod_ = boost::regex_search(expression_, what, expr);
-    
-    // If method is compound, use CINT to evaluate it
-    
-    if ( compoundMethod_ )
-      {
-        if ( debug_ > 0 )
-          std::cout << "Method - " 
-                    << RED << "compound method" 
-                    << BLACK 
-                    << std::endl;
-        
-        std::string delim(what[0]);
-
-        if ( debug_ > 0 )
-          std::cout << "         delimeter: (" << delim << ")" << std::endl;
-
-        kit::bisplit(expression_, expression1_, expression2_, delim);
-
-        // Get return type of 1st part of compound method
-        // and determine if it is a pointer 
-
-        std::vector<kit::ValueThing*> values;
-        std::vector<void*> args;
-        Reflex::Member method = kit::decodeMethod(classname_,
-                                                  expression1_,
-                                                  values,
-                                                  args);
-        if ( !kit::memberValid(method) )
-          throw edm::Exception(edm::errors::Configuration,
-                               "I'm too stupid to decode \"" 
-                               + RED + expression1_ + BLACK + "\"\n");
-
-        // Command to set address of object
-        char cmd[1024];
-
-        sprintf(cmd, "gROOT->Reset(); %s* o = (%s*)0x%s", 
-                classname_.c_str(), classname_.c_str(), "%x");
-        setAddress_ = std::string(cmd);
-
-        // Check return type 
-        if ( kit::returnsPointer(method) )
-          {
-            // Returns a simple pointer
-            checkReturn_ = true;
-            sprintf(cmd, "o->%s", expression1_.c_str());
-          }
-        else
-          {
-            Reflex::Member isNull = kit::getisNull(method); 
-            if ( kit::memberValid(isNull) )
-              {
-                checkReturn_ = true;
-                sprintf(cmd, "o->%s.get()", expression1_.c_str());
-              }
-            else
-              {
-                sprintf(cmd, "%s", "");
-              }
-          }
-        callMethod1_ = std::string(cmd);
-
-        sprintf(cmd, "kit::set(o->%s, x)", expression_.c_str());
-        compoundCall_ = std::string(cmd);
-      }
-    else
-      {
-        if ( debug_ > 0 )
-          std::cout << "Method - " 
-                    << RED << "simple method" 
-                    << BLACK 
-                    << std::endl;
-
-        // This is a simple method, so use
-        // expression parser to evaluate it
-
-        expression1_ = expression_;
-        if(!reco::parser::expressionParser<T>(expression1_, expr1_)) 
-          {
-            throw edm::Exception(edm::errors::Configuration,
-                                 "Since I'm a cyber ignoramous, "
-                                 "I'm too stupid to understand \"" 
-                                 + RED + expression1_ + BLACK + "\"\n");
-          }
-      }
-
-    if ( debug_ > 0 )
-      std::cout << "Method - 1: " << expression1_ << std::endl
-                << "         2: " << expression2_ << std::endl;
-    
-  }
-  
-  std::string name() const
-  {
-    return std::string(boost::python::type_id<T>().name()) 
-      + "/" + expression_;
-  }
-
+  ///
   double operator()(const T& t)
   {
-    using namespace ROOT::Reflex;
-
-    if ( !compoundMethod_ )
-      {
-        if ( debug_ > 0 )
-          //DB
-          std::cout << "simpleMethod(" << classname_ << "): " 
-                    << GREEN 
-                    << expression_
-                    << BLACK
-                    << std::endl;
-
-        Object o(type_, const_cast<T*>(&t));
-        double x = expr1_->value(o);
-
-        if ( debug_ > 0 )
-          //DB
-          std::cout << "  return: " 
-                    << RED 
-                    << x
-                    << BLACK
-                    << std::endl;
-        return x;
-      }
-    else
-      {
-        if ( debug_ > 0 )
-          std::cout << "compoundMethod(" << classname_ << "): " 
-                    << RED 
-                    << expression_
-                    << BLACK
-                    << std::endl
-                    << " setAddress: " 
-                    << RED 
-                    << setAddress_
-                    << BLACK
-                    << std::endl;
-
-        gROOT->ProcessLine(Form(setAddress_.c_str(), &t)); 
-        if ( checkReturn_ )
-          {
-            if ( debug_ > 0 )
-              //DB
-              std::cout << "   checkReturn: " 
-                        << RED 
-                        << callMethod1_
-                        << BLACK
-                        << std::endl;
-            void* y = 0;
-            try
-              {
-                y = (void*)gROOT->ProcessLineFast(callMethod1_.c_str());
-              }
-            catch (...)
-              {}
-
-            if ( y == 0 )
-              {
-                double x = -99999;
-            
-                if ( debug_ > 0 )
-                  //DB
-                  std::cout << "     return: " 
-                            << RED 
-                            << x
-                            << BLACK
-                            << std::endl;
-                return x;
-              }
-          }
-
-        // We have a valid pointer, so proceed
-
-        double x = 0;
-        gROOT->ProcessLine(Form("double* x = (double*)0x%x", &x));
-
-        if ( debug_ > 0 )
-          //DB
-          std::cout << "     callCompound: " 
-                    << RED 
-                    << compoundCall_
-                    << BLACK
-                    << std::endl;
-
-        try
-          {
-            gROOT->ProcessLineFast(compoundCall_.c_str());
-          }
-        catch (...)
-          {}
-
-        if ( debug_ > 0 )
-          //DB
-          std::cout << "       return: " 
-                    << RED 
-                    << x
-                    << BLACK
-                    << std::endl;
-        return x;
-      }
+    void* address = static_void(void*)(&t);
+    ROOT::Reflex::Object object(type_, const_cast<T*>(address));
+    return (*this)(object, address);
   }
-  
-private:
-
-  std::string expression_;
-  std::string expression1_;
-  std::string expression2_;
-  std::string classname_;
-  ROOT::Reflex::Type type_;
-  reco::parser::ExpressionPtr expr1_;
-
-  bool compoundMethod_;
-  bool checkReturn_;
-
-  std::string setAddress_;
-  std::string callMethod1_;
-  std::string compoundCall_;
-
-  int debug_;
 };
 
 #endif
---
+++ PhysicsTools/LiteAnalysis/src/MethodTBase.cc
@@ -0,0 +1,276 @@
+// -*- C++ -*-
+//
+// Package:    PhysicsTools/LiteAnalysis
+// Class:      MethodTBase
+// 
+/**\class MethodTBase MethodTBase.cc 
+   PhysicsTools/LiteAnalysis/src/MethodBase.cc
+   Description: Base class for MethodT
+
+   Model function members, that is, methods.
+    This is the base class of the template class MethodT. The functions that 
+    can be modeled must return simple types and have either no arguments or
+    arguments comprising simple types. The functions can be of the form:
+    - y = method1(...)->method2(...) or
+    - y = method1(...).method2(...) or
+    - y = method1(..).variable
+
+   Implementation:
+   Common sense and a sense of beauty.
+*/
+//
+// Original Author:  Harrison B. Prosper
+//         Created:  Tue Dec  8 15:40:26 CET 2009
+// $Id: MethodT.h,v 1.2 2010/09/06 05:28:48 prosper Exp $
+//
+// If using Python, include its header first to avoid annoying compiler
+// complaints.
+// ---------------------------------------------------------------------------
+#include <Python.h>
+#include <boost/python/type_id.hpp>
+#include <boost/regex.hpp>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <cassert>
+#include <stdlib.h>
+
+#include "CommonTools/Utils/src/ExpressionPtr.h"
+#include "CommonTools/Utils/src/ExpressionBase.h"
+#include "CommonTools/Utils/interface/expressionParser.h"
+#include "PhysicsTools/LiteAnalysis/interface/kit.h"
+#include "PhysicsTools/LiteAnalysis/interface/MethodTBase.h"
+#include "TROOT.h"
+// ---------------------------------------------------------------------------
+using namespace ROOT::Reflex;
+
+bool isCompoundMethod(std::string expression, std::string& delim)
+{
+  // method could be of the form
+  // y = method1(...)->method2(...) or
+  // y = method1(...).method2(...) or
+  // y = method1(..).variable
+  boost::regex expr("(?<=[)]) *([-][>]|[.]) *(?=[a-zA-Z])");
+  boost::smatch what;
+  bool yes = boost::regex_search(expression, what, expr);
+  if ( yes ) delim = what[0];
+  return yes;
+}
+
+MethodTBase::MethodTBase() {}
+  
+MethodTBase::MethodTBase(const std::string classname, 
+                         const std::string expression,
+                         reco::parser::ExpressionPtr& expr)
+    : classname_(classname),
+      expression_(expression),
+      expr1_(expr),
+      expression1_(""),
+      expression2_(""),
+      compoundMethod_(false),
+      checkReturn_(false),
+      checkisNull_(false)
+{
+  if ( getenv("DEBUGMETHOD") > 0 )
+    debug_ = atoi(getenv("DEBUGMETHOD"));
+  else
+    debug_ = 0;
+  
+  if ( debug_ > 0 )
+    std::cout << "Expression( " 
+              << BLUE << expression_ <<  BLACK << " )" << std::endl;
+  
+  std::string delim("");
+  compoundMethod_ = isCompoundMethod(expression_, delim);
+  
+  // If method is compound, use CINT to evaluate it
+  // otherwise use the expression parser
+    
+  if ( compoundMethod_ )
+    {
+      if ( debug_ > 0 )
+        std::cout << "Method - " 
+                  << RED << "compound method" 
+                  << BLACK 
+                  << std::endl;
+     
+      if ( debug_ > 0 )
+        std::cout << "         delimeter: (" << delim << ")" << std::endl;
+      
+      kit::bisplit(expression_, expression1_, expression2_, delim);
+      
+      // Get return type of 1st part of compound method
+      // and determine if it is a pointer 
+
+      std::vector<kit::ValueThing*> values;
+      std::vector<void*> args;
+      Reflex::Member method = kit::decodeMethod(classname_,
+                                                expression1_,
+                                                values,
+                                                args);
+      if ( !kit::memberValid(method) )
+        throw edm::Exception(edm::errors::Configuration,
+                             "I'm too stupid to decode \"" 
+                             + expression1_ + "\"\n");
+      
+      // buffer for commands
+      char cmd[1024];
+
+      // Command to set address of object
+      sprintf(cmd, "gROOT->Reset(); %s* o = (%s*)0x%s", 
+              classname_.c_str(), classname_.c_str(), "%x");
+      setAddress_ = std::string(cmd);
+      
+      // Check return type 
+      if ( kit::returnsPointer(method) )
+        {
+          // Returns a simple pointer
+          checkReturn_ = true;
+          sprintf(cmd, "o->%s", expression1_.c_str());
+        }
+      else
+        {
+          // Check for return of objects with an isNull() method
+          Reflex::Member isNull = kit::getisNull(method); 
+          if ( kit::memberValid(isNull) )
+            {
+              checkReturn_ = true;
+              checkisNull_ = true;
+              // Command to call isNull()
+              sprintf(cmd, "kit::set(o->%s.isNull(), x)", 
+                      expression1_.c_str());
+            }
+          else
+            {
+              sprintf(cmd, "%s", "");
+            }
+        }
+      callMethod1_ = std::string(cmd);
+      
+      sprintf(cmd, "kit::set(o->%s, x)", expression_.c_str());
+      compoundCall_ = std::string(cmd);
+    }
+  else
+    {
+      if ( debug_ > 0 )
+        std::cout << "Method - " 
+                  << RED << "simple method" 
+                  << BLACK 
+                  << std::endl;
+      
+      // This is a simple method, so use
+      // expression parser to evaluate it
+      
+      expression1_ = expression_;
+    }
+
+  if ( debug_ > 0 )
+    std::cout << "Method - 1: " << expression1_ << std::endl
+              << "         2: " << expression2_ << std::endl;
+}
+
+std::string MethodTBase::name() const
+{
+  return classname_ + "::" + expression_;
+}
+
+double MethodTBase::operator()(ROOT::Reflex::Object& object, void* address)
+{
+  if ( !compoundMethod_ )
+    {
+      if ( debug_ > 0 )
+        //DB
+        std::cout << "simpleMethod(" << classname_ << "): " 
+                  << GREEN 
+                  << expression_
+                  << BLACK
+                  << std::endl;
+
+      double x = expr1_->value(object);
+      
+      if ( debug_ > 0 )
+        //DB
+        std::cout << "  return: " 
+                  << RED 
+                  << x
+                  << BLACK
+                  << std::endl;
+      return x;
+    }
+  else
+    {
+      if ( debug_ > 0 )
+        std::cout << "compoundMethod(" << classname_ << "): " 
+                  << RED 
+                  << expression_
+                  << BLACK
+                  << std::endl
+                  << " setAddress: " 
+                  << RED 
+                  << setAddress_
+                  << BLACK
+                  << std::endl;
+      
+      // Initialize pointer to address of object
+      gROOT->ProcessLine(Form(setAddress_.c_str(), address));
+      
+      if ( checkReturn_ )
+        {          
+          if ( checkisNull_ )
+            {
+              if ( debug_ > 0 )
+                //DB
+                std::cout << "   call isNull(): " 
+                          << std::endl;
+
+              bool valid=false;
+              gROOT->ProcessLine(Form("bool* x = (bool*)0x%x", &valid));
+              gROOT->ProcessLineFast(callMethod1_.c_str());
+              
+              if ( debug_ > 0 )
+                //DB
+                std::cout << "        return: " 
+                          << GREEN 
+                          << valid
+                          << BLACK
+                          << std::endl;
+
+              if ( !valid ) return 0;
+            }
+          else
+            {
+              void* y = 0;
+              y = (void*)gROOT->ProcessLineFast(callMethod1_.c_str());
+              if ( y == 0 ) return 0;
+            }
+        }
+      
+      // We have a valid pointer, so proceed
+
+      double x = 0;
+      gROOT->ProcessLine(Form("double* x = (double*)0x%x", &x));
+      
+      if ( debug_ > 0 )
+        //DB
+        std::cout << "     callCompound: " 
+                  << RED 
+                  << compoundCall_
+                  << BLACK
+                  << std::endl;
+      try
+        {
+          gROOT->ProcessLineFast(compoundCall_.c_str());
+        }
+      catch (...)
+        {}
+      
+      if ( debug_ > 0 )
+        //DB
+        std::cout << "       return: " 
+                  << RED 
+                  << x
+                  << BLACK
+                  << std::endl;
+      return x;
+    }
+}
